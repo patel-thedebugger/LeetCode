30e45a044a5470523abb5580d038a591bdb8e93b
Patel Garv <142311401+patel-thedebugger@users.noreply.github.com>
2026-03-06 14:41:45 +0530

    Time: 305 ms (34.42%) | Memory: 12.9 MB (24.40%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -9,6 +9,8 @@ class Solution(object):
         for i in n :
             if(max < int(i)):
                 max = int(i)
+            if(max == 9):
+                return 9
         
         return max
         
